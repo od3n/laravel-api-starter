c2a76d4113261370913f075400af9cefb190d039
Salahuddin Hairai <mr.od3n@gmail.com>
2015-11-15 18:09:37 +0800

    Add index route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,13 +7,18 @@ $api->version('v1', function ($api) {
 	// Set our namespace for the underlying routes
 	$api->group(['namespace' => 'Api\Controllers', 'middleware' => 'cors'], function ($api) {
 
+		// Index route
+		$api->get('/', function () {
+			return response()->json(['Welcome to Laravel API Starter']);
+		});
+
 		// Login route
 		$api->post('login', 'AuthController@authenticate');
 		$api->post('register', 'AuthController@register');
 
 		// Cats! All routes in here are protected and thus need a valid token
-		//$api->group( [ 'protected' => true, 'middleware' => 'jwt.refresh' ], function ($api) {
-		$api->group( [ 'middleware' => 'jwt.refresh' ], function ($api) {
+		//$api->group([ 'protected' => true, 'middleware' => 'jwt.refresh'], function ($api) {
+		$api->group([ 'middleware' => 'jwt.refresh'], function ($api) {
 
 			$api->get('users/me', 'AuthController@me');
 			$api->get('validate_token', 'AuthController@validateToken');
